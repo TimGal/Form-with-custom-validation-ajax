176984c76849828d80165ce27e1c48a4be21245c
Timur Galdin <galdin.timur508@gmail.com>
2020-03-22 15:57:02 +0300

    Added custom email/password validation

---
+++ app/js/ext.js
@@ -0,0 +1,65 @@
+document.querySelector('.form-signup').addEventListener('submit', validateForm);
+
+function validateForm(event) {
+  event.preventDefault();
+  console.log('проверка формы');
+  let form = document.querySelector('.form-signup') //находим форму
+  let btn = document.querySelector('.btn-submit'); //находим кнопку
+  let email = document.querySelector('#email').value; // Получаем email из формы
+  let password = form.password.value; // Получаем пароль из формы
+
+  console.log('email:' + ' ' + email);
+  console.log('pass:' + ' ' + password);
+  function validateEmail() { // делаем проверку e-mail
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // регулярное выражение для e-mail
+    if (re.test()) {
+      return true;
+    } else if (email.length != 0) {
+      alert('invalid email');
+      return false;
+    } else {
+      alert('Please input email');
+      return false;
+    }
+
+
+
+  };
+
+  validateEmail();
+
+  function checkPassword() {
+     let s_letters = "qwertyuiopasdfghjklzxcvbnm"; // Буквы в нижнем регистре
+     let b_letters = "QWERTYUIOPLKJHGFDSAZXCVBNM"; // Буквы в верхнем регистре
+     let digits = "0123456789"; // Цифры
+     let specials = "!@#$%^&*()_-+=\|/.,:;[]{}"; // Спецсимволы
+     let is_s = false; // Есть ли в пароле буквы в нижнем регистре
+     let is_b = false; // Есть ли в пароле буквы в верхнем регистре
+     let is_d = false; // Есть ли в пароле цифры
+     let is_sp = false; // Есть ли в пароле спецсимволы
+     for (let i = 0; i < password.length; i++) {
+       /* Проверяем каждый символ пароля на принадлежность к тому или иному типу */
+       if (!is_s && s_letters.indexOf(password[i]) != -1) is_s = true;
+       else if (!is_b && b_letters.indexOf(password[i]) != -1) is_b = true;
+       else if (!is_d && digits.indexOf(password[i]) != -1) is_d = true;
+       else if (!is_sp && specials.indexOf(password[i]) != -1) is_sp = true;
+     }
+     let rating = 0;
+     let text = "";
+     if (is_s) rating++; // Если в пароле есть символы в нижнем регистре, то увеличиваем рейтинг сложности
+     if (is_b) rating++; // Если в пароле есть символы в верхнем регистре, то увеличиваем рейтинг сложности
+     if (is_d) rating++; // Если в пароле есть цифры, то увеличиваем рейтинг сложности
+     if (is_sp) rating++; // Если в пароле есть спецсимволы, то увеличиваем рейтинг сложности
+     /* Далее идёт анализ длины пароля и полученного рейтинга, и на основании этого готовится текстовое описание сложности пароля */
+     if (password.length < 6 && rating < 3) text = "Простой";
+     else if (password.length < 6 && rating >= 3) text = "Средний";
+     else if (password.length >= 8 && rating < 3) text = "Средний";
+     else if (password.length >= 8 && rating >= 3) text = "Сложный";
+     else if (password.length >= 6 && rating == 1) text = "Простой";
+     else if (password.length >= 6 && rating > 1 && rating < 4) text = "Средний";
+     else if (password.length >= 6 && rating == 4) text = "Сложный";
+     alert(text); // Выводим итоговую сложность пароля
+     return false; // Форму не отправляем
+   };
+
+};
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,135 @@
-console.log('Hello test!');
+let form = document.querySelector('.form-signup') //находим форму
+let btn = document.querySelector('.btn-submit'); //находим кнопку
+let email = document.querySelector('#email').value; // Получаем email из формы
+let password = document.querySelector('#password').value; // Получаем пароль из формы
+let confirm_password = document.querySelector('#confirm_password').value;// Получаем подтверждение пароля из формы
+
+function getPasswordValue() {
+  return document.querySelector('#password').value; // Получаем пароль из формы
+}
+
+function getConfirmPasswordValue() {
+  return document.querySelector('#confirm_password').value; // Получаем пароль из формы
+}
+
+function checkEmailValid() {
+   var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   var address = email;
+   if(reg.test(address) == false) {
+      return false;
+   } else {
+     return true;
+   }
+};
+
+function checkPasswordHasSmLetters () {
+  let password = getPasswordValue();
+  let s_letters = "qwertyuiopasdfghjklzxcvbnm"; // Буквы в нижнем регистре
+  let is_s = false; // Есть ли в пароле буквы в нижнем регистре
+  for (let i = 0; i < password.length; i++) {
+    if (s_letters.indexOf(password[i]) != -1) is_s = true;
+  }
+  return is_s;
+}
+
+function checkPasswordHasBigLetters () {
+  let password = getPasswordValue();
+  let b_letters = "QWERTYUIOPLKJHGFDSAZXCVBNM"; // Буквы в верхнем регистре
+  let is_b = false; // Есть ли в пароле буквы в верхнем регистре
+  for (let i = 0; i < password.length; i++) {
+    if (b_letters.indexOf(password[i]) != -1) is_b = true;
+  }
+  return is_b;
+}
+
+function checkPasswordHasDigits () {
+  let password = getPasswordValue();
+  let digits = "0123456789"; // Цифры
+  let is_d = false; // Есть ли в пароле цифры
+  for (let i = 0; i < password.length; i++) {
+    if ( digits.indexOf(password[i]) != -1) is_d = true;
+  }
+  return is_d;
+}
+
+function checkPasswordLongEnough () {
+  let password = getPasswordValue();
+  if (password.length > 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkPasswordConfirm() {
+  let password = getPasswordValue();
+  let confirm_password = getConfirmPasswordValue();
+  if (password == confirm_password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function CustomValidation() { }
+
+CustomValidation.prototype = {
+  // Установим пустой массив сообщений об ошибках
+  invalidities: [],
+
+  // Метод, проверяющий валидность
+  checkValidity: function(input) {
+
+    var validity = input.validity;
+
+    if (validity.patternMismatch) {
+      this.addInvalidity('This is the wrong pattern for this field');
+    }
+
+    if (validity.rangeOverflow) {
+      var max = getAttributeValue(input, 'max');
+      this.addInvalidity('The maximum value should be ' + max);
+    }
+
+    if (validity.rangeUnderflow) {
+      var min = getAttributeValue(input, 'min');
+      this.addInvalidity('The minimum value should be ' + min);
+    }
+
+    if (validity.stepMismatch) {
+      var step = getAttributeValue(input, 'step');
+      this.addInvalidity('This number needs to be a multiple of ' + step);
+    }
+
+    // И остальные проверки валидности...
+  },
+
+  // Добавляем сообщение об ошибке в массив ошибок
+  addInvalidity: function(message) {
+    this.invalidities.push(message);
+  },
+
+  // Получаем общий текст сообщений об ошибках
+  getInvalidities: function() {
+    return this.invalidities.join('. \n');
+  }
+};
+
+// Добавляем обработчик клика на кнопку отправки формы
+submit.addEventListener('click', function(e) {
+  // Пройдёмся по всем полям
+  for (var i = 0; i < inputs.length; i++) {
+
+    var input = inputs[i];
+
+    // Проверим валидность поля, используя встроенную в JavaScript функцию checkValidity()
+    if (input.checkValidity() == false) {
+
+      var inputCustomValidation = new CustomValidation(); // Создадим объект CustomValidation
+      inputCustomValidation.checkValidity(input); // Выявим ошибки
+      var customValidityMessage = inputCustomValidation.getInvalidities(); // Получим все сообщения об ошибках
+      input.setCustomValidity(customValidityMessage); // Установим специальное сообщение об ошибке
+
+    } // закончился if
+  } // закончился цикл
+});
